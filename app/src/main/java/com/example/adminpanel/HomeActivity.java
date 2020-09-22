57610548025c55a6d701d49bf78ec3bbbddb8d03
huzaifamalik47 <huzaifamalik47@gmail.com>
2020-09-22 13:17:39 +0500

    added login and forgetpass activity

--- app/src/main/java/com/example/adminpanel/HomeActivity.java
+++ app/src/main/java/com/example/adminpanel/HomeActivity.java
@@ -1,14 +1,52 @@
 package com.example.adminpanel;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public DrawerLayout drawerLayout;
+    public NavigationView navigationView;
+    public Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+//        imageView1 = findViewById(R.id.menu1);
+//        imageView2 = findViewById(R.id.menu2);
+//        imageView3 = findViewById(R.id.menu3);
+//        imageView1.setOnClickListener(this);
+//        imageView2.setOnClickListener(this);
+//        imageView3.setOnClickListener(this);
+//        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+        navigationView.bringToFront();
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+//        toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
