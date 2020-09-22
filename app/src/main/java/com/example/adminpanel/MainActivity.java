7bd1375b14ed016d1ac2ca759bc0046475793bd6
huzaifamalik47 <huzaifamalik47@gmail.com>
2020-09-22 12:34:35 +0500

    added login and forgetpass activity

--- app/src/main/java/com/example/adminpanel/MainActivity.java
+++ app/src/main/java/com/example/adminpanel/MainActivity.java
@@ -1,31 +1,106 @@
 package com.example.adminpanel;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity  {
     EditText e1, e2;
     Button b1;
+    MaterialTextView materialTextView;
     public static final String TAG = "mytag";
-    String message;
-    String savedata;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1 = findViewById(R.id.button);
-        e1 = findViewById(R.id.user_id);
-        e2 = findViewById(R.id.pass);
-        b1.setOnClickListener(this);
+        b1 = findViewById(R.id.signButton);
+        e1 = findViewById(R.id.emailText);
+        materialTextView = findViewById(R.id.forgetPassword);
+        e2 = findViewById(R.id.passwordText);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     @Override
-    public void onClick(View v) {
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+        materialTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forgetPassword();
+            }
+        });
+
+
+    }
+
+    private void forgetPassword() {
+        Intent intent=new Intent(getApplicationContext(),forgetPassword.class);
+        startActivity(intent);
+    };
+
+    public void signIn() {
+        String email = e1.getText().toString().trim();
+        String pass = e2.getText().toString().trim();
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        firebaseAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(getApplicationContext(), "You have been Logged in", Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+//                            Intent intent = new Intent(getApplicationContext(), drawer_act.class);
+//                            startActivity(intent);
+//                            customType(login.this, "bottom-to-up");
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                            // ...
+                        }
+
+                        // ...
+                    }
+                });
 
     }
 }
---
+++ app/src/main/java/com/example/adminpanel/forgetPassword.java
@@ -0,0 +1,55 @@
+package com.example.adminpanel;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class forgetPassword extends AppCompatActivity {
+    private EditText editText;
+    private Button forget_button;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+        editText = findViewById(R.id.emailText_forget);
+        forget_button = findViewById(R.id.forgetPassword_new);
+        forget_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forget();
+            }
+        });
+    }
+
+    private void forget() {
+        String email = editText.getText().toString().trim();
+        if (email.length() != 0) {
+            firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+
+
+            Toast.makeText(getApplicationContext(), "Enter the Email", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
