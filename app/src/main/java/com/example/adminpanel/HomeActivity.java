b8f3c54d6d9962c4f0b52d98161e452b425f1c17
huzaifamalik47 <huzaifamalik47@gmail.com>
2020-09-23 11:19:24 +0500

    added login and forgetpass activity added classes+onClick listeners

--- app/src/main/java/com/example/adminpanel/HomeActivity.java
+++ app/src/main/java/com/example/adminpanel/HomeActivity.java
@@ -1,24 +1,29 @@
 package com.example.adminpanel;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.graphics.Color;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ViewSwitcher;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
     public DrawerLayout drawerLayout;
     public NavigationView navigationView;
     public Toolbar toolbar;
+    ImageView imageView1, imageView2, imageView3, imageView4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +31,14 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawerLayout = findViewById(R.id.drawer);
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.toolbar);
-//        imageView1 = findViewById(R.id.menu1);
-//        imageView2 = findViewById(R.id.menu2);
-//        imageView3 = findViewById(R.id.menu3);
-//        imageView1.setOnClickListener(this);
-//        imageView2.setOnClickListener(this);
-//        imageView3.setOnClickListener(this);
-//        navigationView = findViewById(R.id.nav_view);
+        imageView1 = findViewById(R.id.dramaView);
+        imageView2 = findViewById(R.id.trendingView);
+        imageView3 = findViewById(R.id.moviesView);
+        imageView4 = findViewById(R.id.slidersView);
+        imageView1.setOnClickListener(this);
+        imageView2.setOnClickListener(this);
+        imageView3.setOnClickListener(this);
+        imageView4.setOnClickListener(this);
         toolbar = findViewById(R.id.toolbar);
         navigationView.bringToFront();
         setSupportActionBar(toolbar);
@@ -47,6 +53,80 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        return false;
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Toast.makeText(getApplicationContext(), "Home", 0).show();
+                break;
+            case R.id.drama:
+//                Intent browserIntent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hamaradramasinurdu/?hl=en"));
+//                startActivity(browserIntent1);
+                Toast.makeText(getApplicationContext(), "Home", 0).show();
+
+                break;
+            case R.id.movies:
+//                Intent browserIntent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/HMKahania"));
+//                startActivity(browserIntent2);
+                Toast.makeText(getApplicationContext(), "Home", 0).show();
+
+                break;
+            case R.id.sliders:
+//                Intent browserIntent3 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com"));
+//                startActivity(browserIntent3);
+                Toast.makeText(getApplicationContext(), "Home", 0).show();
+
+                break;
+            case R.id.trending:
+                Toast.makeText(getApplicationContext(), "Rate App on Playstore", 0).show();
+                Toast.makeText(getApplicationContext(), "Home", 0).show();
+
+                break;
+            case R.id.logout:
+                finish();
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to exit?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            HomeActivity.this.finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.dramaView) {
+            Intent intent = new Intent(getApplicationContext(), addDrama.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.moviesView) {
+            Intent intent = new Intent(getApplicationContext(), addMovie.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.slidersView) {
+            Intent intent = new Intent(getApplicationContext(), addSlider.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.trendingView) {
+            Intent intent = new Intent(getApplicationContext(), addTrending.class);
+            startActivity(intent);
+        }
+
     }
 }
